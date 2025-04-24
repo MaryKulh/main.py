9b09d112f7cc6a35ec435d3148d820d6db6fccd9
MaryKulh <marie.paskova@seznam.cz>
2025-04-24 19:18:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,110 +1,61 @@
 print("""
-projekt_1.py: první projekt do Engeto Online Python Akademie
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
 
 author: Maryna Kulhánková
 email: marie.paskova@seznam.cz
 """)
 
-
-TEXTS = [
-    '''Situated about 10 miles west of Kemmerer,
-    Fossil Butte is a ruggedly impressive
-    topographic feature that rises sharply
-    some 1000 feet above Twin Creek Valley
-    to an elevation of more than 7500 feet
-    above sea level. The butte is located just
-    north of US 30 and the Union Pacific Railroad,
-    which traverse the valley.''',
-    '''At the base of Fossil Butte are the bright
-    red, purple, yellow and gray beds of the Wasatch
-    Formation. Eroded portions of these horizontal
-    beds slope gradually upward from the valley floor
-    and steepen abruptly. Overlying them and extending
-    to the top of the butte are the much steeper
-    buff-to-white beds of the Green River Formation,
-    which are about 300 feet thick.''',
-    '''The monument contains 8198 acres and protects
-    a portion of the largest deposit of freshwater fish
-    fossils in the world. The richest fossil fish deposits
-    are found in multiple limestone layers, which lie some
-    100 feet below the top of the butte. The fossils
-    represent several varieties of perch, as well as
-    other freshwater genera and herring similar to those
-    in modern oceans. Other fish such as paddlefish,
-    garpike and stingray are also present.'''
-]
+import random
 
 cara = "-" * 40
-print(cara)
 
-registered_users = {
-    "bob": "123",
-    "mike": "password123",
-    "liz": "pass123",
-    "ann": "pass123",
-}
+def uvod():
+    print(cara)
+    print("Hi there!")
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print(cara)
 
-username = input("username: ")
-password = input("password: ")
+def generuj_tajne_cislo():
+    cisla = list("123456789")
+    nahodne = random.sample(cisla, 4)
+    return "".join(nahodne)
 
-if username in registered_users and registered_users[username] == password:
-    print(f"""Welcome to the app {username},
-    We have 3 texts to be analyzed.
-    """)
-else:
-    print("Your username or password is incorrect!")
-    print("Unregistered user, terminating the program..")
-    exit()
+def validuj_vstup(vstup):
+    if not vstup.isdigit() or len(vstup) != 4:
+        return False
+    if len(set(vstup)) != 4:
+        return False
+    if vstup[0] == "0":
+        return False
+    return True
 
+def vyhodnot_hadani(hadane, tajne):
+    bulls = sum(1 for x, y in zip(hadane, tajne) if x == y)
+    cows = sum(1 for x in hadane if x in tajne) - bulls
+    return bulls, cows
 
-def analyze_text(text):
-    words = text.split()
-    num_words = len(words)
-    num_capitalized = sum(1 for word in words if word[0].isupper())
-    num_uppercase = sum(1 for word in words if word.isupper())
-    num_lowercase = sum(1 for word in words if word.islower())
-    num_numbers = sum(1 for word in words if word.isdigit())
-    sum_numbers = sum(int(word) for word in words if word.isdigit())
+def main():
+    uvod()
+    tajne_cislo = generuj_tajne_cislo()
+    pokusy = 0
+    while True:
+        print(cara)
+        hadane_cislo = input("Enter a number:\n>>> ")
+        print(cara)
+        if not validuj_vstup(hadane_cislo):
+            print("Invalid input, try again.")
+            continue
+        pokusy += 1
+        bulls, cows = vyhodnot_hadani(hadane_cislo, tajne_cislo)
+        print(f"{bulls} bull{'s' if bulls != 1 else ''}, {cows} cow{'s' if cows != 1 else ''}")
+        if bulls == 4:
+            print(cara)
+            print(f"Correct, you've guessed the right number\nin {pokusy} guesses!")
+            print(cara)
+            print("That's amazing!")
+            break
 
-    print(f"\nThere are {num_words} words in the selected text.")
-    print(f"There are {num_capitalized} titlecase words.")
-    print(f"There are {num_uppercase} uppercase words.")
-    print(f"There are {num_lowercase} lowercase words.")
-    print(f"There are {num_numbers} numeric strings.")
-    print(f"The sum of all the numbers {sum_numbers}")
-
-
-choice = input("Enter a number btw. 1 and 3 to select: ")
-
-if choice.isdigit():
-    choice = int(choice)
-    if 1 <= choice <= 3:
-        selected_text = TEXTS[choice - 1]
-        analyze_text(selected_text)
-    else:
-        print("Invalid number. Exiting.")
-else:
-    print("Invalid input. Exiting.")
-
-print(cara)
-
-
-length_counts = {}
-
-for text in TEXTS:
-    words = text.split()
-    for word in words:
-        length = len(word)
-        if length in length_counts:
-            length_counts[length] += 1
-        else:
-            length_counts[length] = 1
-
-
-print(f"{'LEN':<4}| {'OCCURRENCES':<36}| {'NR.':>3}")
-print(cara)
-for length in sorted(length_counts.keys()):
-    count = length_counts[length]
-    stars = "*" * count
-    print(f"{length:<4}| {stars:<36}| {count:>3}")
+if __name__ == "__main__":
+    main()
 
